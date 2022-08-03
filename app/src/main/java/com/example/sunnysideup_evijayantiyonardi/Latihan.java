f8fc68d8e0d4251f7116ad99382b5c3936931515
EviYnrd <evi.jayanti.yonardi6@gmail.com>
2022-08-03 12:31:13 +0800

    anjing

--- app/src/main/java/com/example/sunnysideup_evijayantiyonardi/Latihan.java
+++ app/src/main/java/com/example/sunnysideup_evijayantiyonardi/Latihan.java
@@ -10,5 +10,7 @@ public class Latihan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_latihan);
+
+//anjing
     }
 }
